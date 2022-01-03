85635e9ec57e77861673c304dbf39cf597cf5438
monstermiles <miles.emmitt.moss@gmail.com>
2022-01-03 16:41:24 -0600

    got delete function working

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ app.use(express.json());
 const id = Math.floor(Math.random() * 1000);
 
 
+
+
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'public.index.html'))
+    res.sendFile(path.join(__dirname, 'public.index.html'))
 });
 
 ///////////////////////////////Get req, sends notes.html///////////////////////////////
@@ -42,7 +44,7 @@ app.get('/notes', (req, res) => {
 //     },
 //   });
 
-app.get('/api/notes', (req, res) =>{
+app.get('/api/notes', (req, res) => {
     res.status(200).json(savedNotes)
 })
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -63,21 +65,22 @@ app.post('/api/notes', (req, res) => {
     console.log(`${req.method} request received.`);
     // console.log(req.body)
     const { title, text } = req.body
-
+    
+    
     const newNote = {
         title,
         text,
         id
     }
-    
+
     savedNotes.push(newNote)
     // console.log(savedNotes)
 
     const stringNotes = JSON.stringify(savedNotes)
-   
+
 
     fs.writeFile('./db/db.json', stringNotes, (err) =>
-    err ? console.error("There was an error adding a note.") : console.log('Note saved!'))
+        err ? console.error("There was an error adding a note.") : console.log('Note saved!'))
 
 
     res.json(savedNotes);
@@ -94,44 +97,48 @@ app.post('/api/notes', (req, res) => {
 //     },
 //   });
 
-app.delete(`/api/notes/:id`, (req, res) =>{
-    const saveArray = []
+app.delete(`/api/notes/:id`, (req, res) => {
+
+    // const saveArray = savedNotes.filter(note => console.log(note.id))  //<<logs id for both notes
+    // console.log(req.params.id)
+
+    // const saveArray = savedNotes.filter(note => note.id !== req.params.id)
+    // console.log(saveArray)  //<< same as savedNotes
+
+
+
+
+
+    // savedNotes.forEach(note => {
+    //     console.log(typeof req.params.id)
+    //     console.log(typeof note.id)
+    //     if (note.id == req.params.id) {
+    //         console.log("THESE NUMBERS ARE THE SAME")
+    //         // console.log(note)  //this logs both notes.
+    //         savedNotes.splice(note[i])
+    //         console.log(savedNotes)
+    //     } 
+    // });
+    // console.log(saveArray)
+
+
+
+    
     for (var i = 0; i < savedNotes.length; i++) {
-        if (savedNotes[i].id !== req.params.id) {
-            saveArray.push(savedNotes[i])
+        console.log("note to delete:", req.params.id)
+        console.log("current note:", savedNotes[i].id)
+        if (savedNotes[i].id == req.params.id) {
+            savedNotes.splice(i,1)
+            console.log(savedNotes)
         }
     }
-    console.log(saveArray)
-    
-    
-    
-    
-    // console.log(req.params.id)
-    // savedNotes.filter(note => console.log(note.id))
-    // const saveArray = savedNotes.filter(note => note.id !== req.params.id)
-    // console.log(saveArray)
-   
-   
-   
-   
-    // savedNotes.forEach(note => {
-    //     console.log(note[])
-    //     // const currentId = note[i].id;
-    //     // if (currentId === req.params.id) {
-    //     //     savedNotes.splice(currentId)
-    //     // }
-    // })
-    // // console.log(savedNotes)
-    // stringNotes = JSON.stringify(savedNotes)
 
-    // // saveArray = savedNotes.filter(note => note.id !== req.params.id)
-    // // console.log(saveArray)
-    // // const stringArray = JSON.stringify(saveArray)
-    
-    // fs.writeFile('./db/db.json', stringNotes , (err) =>
-    // err ? console.error("There was an error deleting a note.") : console.log("Note deleted.")
-    // )
-    
+    stringNotes = JSON.stringify(savedNotes)
+
+    fs.writeFile('./db/db.json', stringNotes , (err) =>
+    err ? console.error("There was an error deleting a note.") : console.log("Note deleted.")
+    )
+
     res.json('note has been deleted')
 })
 
